0da44d72ecd044e8f0311e32aaee3e926f1a0de7
alidakk <alidak583@gmail.com>
2022-02-01 13:49:59 +0300

    create LoginResponse for /login

--- core/responses.py
+++ core/responses.py
@@ -1,18 +1,16 @@
-from datetime import date
+from datetime import date, timedelta
 from typing import Generic, TypeVar
-import uuid
 from pydantic.generics import GenericModel
 from core.models import *
-from sqlalchemy.dialects.postgresql import UUID
 
 T = TypeVar('T')
 class BaseResponse(GenericModel, Generic[T]):
-  info    : Info
+  info    : Info = None
   payload : T = None
 
 
 class UserResponse(BaseModel):
-  userId     : str = ""
+  userId      : str = ""
   email       : str = ""
   firstname   : str = ""
   lastname    : str = ""
@@ -24,5 +22,11 @@ class UserResponse(BaseModel):
     orm_mode = True
   # def __init__(self, user: User): # TODO maybe?
   #   self = self.from_orm(user)
+
+class LoginResponse(BaseModel):
+  access_token : str = ""
+  tokenType    : str = ""
+  userId       : str = ""
+  tokenExpires : timedelta = None  # in seconds
 
 
